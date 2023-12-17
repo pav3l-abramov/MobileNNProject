293765e7ef1f707db45e4b72b2029c02b01e369c
pav3l-abramov <102557961+pav3l-abramov@users.noreply.github.com>
2023-12-17 05:50:23 +0300

    Update train.py

--- train.py
+++ train.py
@@ -3,5 +3,5 @@
 from ultralytics import YOLO
 
 model_yolo = YOLO("yolov8s.pt")
-model_yolo.train(data="custom.yaml", device="cpu", epochs=3, batch=10, imgsz=640,task="detect",mode="train", project="results")
+model_yolo.train(data="custom.yaml", device="cpu", epochs=20, batch=20, imgsz=640,task="detect",mode="train", project="results")
 model_yolo.export(format="torchscript", optimize=True)
